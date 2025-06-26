a7a29b713c23e9465a3654304f5d78ce5b26b101
Dasun Siyasinghe <dasunnilnuwan@gmail.com>
2025-06-27 01:41:25 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/3d-printing-website/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
 });
